1cf09675aa2832593d924b5eb375f33b414a341c
HazelicEyes <henriquepromail9863@gmail.com>
2020-08-18 04:05:39 -0300

    Add project files.

---
+++ array_multi/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace array_multi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[,] notas = new double[2, 4];
+            notas[0 ,0] = 8.0;
+            notas[0, 1] = 7.5;
+            notas[0, 2] = 9.0;
+            notas[0, 3] = 6.5;
+            notas[1, 0] = 5.5;
+            notas[1, 1] = 9.5;
+            notas[1, 2] = 8.5;
+            notas[1, 3] = 7.5;
+
+            MessageBox.Show(notas[0, 1].ToString(), "Array");
+        }
+    }
+}
